4c8f967c58c5e576d8515caeae36eb77740ce21b
sonjiae <117737015+sonjiae@users.noreply.github.com>
2022-12-27 14:07:50 +0900

    init: ANBD 저장소 초기화

---
+++ anabada.zip_expanded/anabada/src/main/java/com/anabada/web/dao/NoteDAOImpl.java
@@ -0,0 +1,89 @@
+package com.anabada.web.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.anabada.web.vo.NoteSearchCriteria;
+import com.anabada.web.vo.NoteVO;
+
+import oracle.net.aso.s;
+
+@Repository
+public class NoteDAOImpl implements NoteDAO{
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	
+	@Override
+	public void send(NoteVO vo) throws Exception {
+		System.out.println("쪽지 보내는 매퍼 직전까지 왔음!!");
+		sqlSession.insert("noteMapper.insert", vo);
+	}
+
+
+	@Override
+	public List<NoteVO> note_list(Map map) throws Exception {
+		return sqlSession.selectList("noteMapper.listPage", map);
+	}
+
+
+	@Override
+	public int note_listCount(Map map) throws Exception {
+		return sqlSession.selectOne("noteMapper.listCount", map);
+	}
+
+
+	@Override
+	public NoteVO note_view(int bno) throws Exception {
+		return sqlSession.selectOne("noteMapper.read", bno);
+	}
+	
+	@Override
+	public void read_check(Map map) throws Exception {
+		sqlSession.selectOne("noteMapper.read_check", map);
+	}
+	
+
+	@Override
+	public int no_readCount(String id) throws Exception {
+		return sqlSession.selectOne("noteMapper.no_readCount", id);
+	}
+
+
+	@Override
+	public void delete_receive(int[] delete_array) throws Exception {
+		sqlSession.update("noteMapper.delete_receive", delete_array);
+	}
+	
+	@Override
+	public void delete_send(int[] delete_array) throws Exception {
+		sqlSession.update("noteMapper.delete_send", delete_array);
+	}
+	
+	@Override
+	public void delete_all(int[] delete_array) throws Exception {
+		sqlSession.delete("noteMapper.delete_all", delete_array);
+	}
+
+	@Override
+	public void delete_send2(int bno) throws Exception {
+		sqlSession.update("noteMapper.delete_send2", bno);
+	}
+
+	@Override
+	public void delete_receive2(int bno) throws Exception {
+		sqlSession.update("noteMapper.delete_receive2", bno);
+	}
+
+	@Override
+	public void delete_detail(int bno) throws Exception {
+		sqlSession.delete("noteMapper.delete_detail", bno);
+	}
+
+}
---
+++ anabada.zip_expanded/anabada/src/main/java/com/anabada/web/service/JobServiceImpl.java
@@ -0,0 +1,76 @@
+package com.anabada.web.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.anabada.web.dao.JobDAO;
+import com.anabada.web.vo.JobCriteria;
+import com.anabada.web.vo.JobSearchCriteria;
+import com.anabada.web.vo.JobVO;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+	@Inject
+	JobDAO dao;
+	
+	// 알바 구인 게시글 작성
+	@Override
+	public void write_boss(JobVO vo) throws Exception {
+		dao.write_boss(vo);
+	}
+
+	// 알바 목록 조회
+	@Override
+	public List<JobVO> job_list(JobSearchCriteria scri) throws Exception {
+		return dao.job_list(scri);
+	}
+
+	// 게시글 총 개수
+	@Override
+	public int job_listCount(JobSearchCriteria scri) throws Exception {
+		return dao.job_listCount(scri);
+	}
+
+	// 알바 구인 게시물 상세 보기
+	@Override
+	public JobVO job_view(int j_bno) throws Exception {
+		return dao.job_view(j_bno);
+	}
+
+	// 알바 구인 게시물 수정 
+	@Override
+	public void update_boss(JobVO vo) throws Exception {
+		dao.update_boss(vo);
+	}
+
+	// 삭제
+	@Override
+	public void delete_boss(int j_bno) throws Exception {
+		dao.delete_boss(j_bno);
+	}
+
+	// 내가 쓴글들 다 보기
+	@Override
+	public List<JobVO> my_jobList(Map map) throws Exception {
+		return dao.my_jobList(map);
+	}
+
+	// 사장이 쓴 알바게시물 갯수
+	@Override
+	public int my_jobListCount(JobCriteria cri) throws Exception {
+		return dao.my_jobListCount(cri);
+	}
+
+	// 삭제할 때 이미지 이름 얻어오는 쿼리
+	@Override
+	public String get_image(int j_bno) throws Exception {
+		return dao.get_image(j_bno);
+	}
+	
+
+}
