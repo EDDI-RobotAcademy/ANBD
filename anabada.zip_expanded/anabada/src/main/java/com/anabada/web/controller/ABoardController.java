93937b6331047ce9f7f0e4d394b88c4970e724c3
englishnee <ywyw4994@gmail.com>
2023-02-03 15:31:14 +0900

    init: [김영언] 관리자가 ‘신고된 게시글’을 삭제 할 때, 해당 게시글 작성자에게 ‘경고를 1회’ 주고 ‘경고성 쪽지’ 보내기 [ANBD-51]

--- anabada.zip_expanded/anabada/src/main/java/com/anabada/web/controller/ABoardController.java
+++ anabada.zip_expanded/anabada/src/main/java/com/anabada/web/controller/ABoardController.java
@@ -1,5 +1,4 @@
 package com.anabada.web.controller;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -18,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -79,114 +79,114 @@ public class ABoardController {
 		return "redirect:/a_board/list";
 		// redirect는 컨트롤러 주소를 찾아감
 	}
-
+	
 	//ckeditor 파일 업로드 
-	 @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
-	    public void imageUpload(HttpServletRequest request, HttpServletResponse response, MultipartHttpServletRequest multiFile
-	    		, @RequestParam MultipartFile upload) throws Exception{
-	    	// 랜덤 문자 생성
-	    	UUID uid = UUID.randomUUID();
-	    	
-	    	OutputStream out = null;
-	    	PrintWriter printWriter = null;
-	    	
-	    	//인코딩
-	    	response.setCharacterEncoding("utf-8");
-	    	response.setContentType("text/html;charset=utf-8");
-	    	try{
-	    		//파일 이름 가져오기
-	    		String fileName = upload.getOriginalFilename();
-	    		byte[] bytes = upload.getBytes();
-	    		
-	    		//이미지 경로 생성
-	    		String path = "C:\\ckeditor_upload_" + "ckImage/";
-	    		String ckUploadPath = path + uid + "_" + fileName;
-	    		File folder = new File(path);
-	    		System.out.println("path : " + path);	// 이미지 저장경로 console에 확인
-	    		
-	    		//해당 디렉토리 확인
-	    		if(!folder.exists()){
-	    			try{
-	    				folder.mkdirs(); // 폴더 생성
-	    		}catch(Exception e){
-	    			e.getStackTrace();
-	    		}
-	    	}
-	    	
-	    	out = new FileOutputStream(new File(ckUploadPath));
-	    	out.write(bytes);
-	    	out.flush(); // outputStram에 저장된 데이터를 전송하고 초기화
-	    	
-	    	String callback = request.getParameter("CKEditorFuncNum");
-	    	printWriter = response.getWriter();
-	    	String fileUrl = "/a_board/ckImgSubmit?uid=" + uid + "&fileName=" + fileName; // 작성화면
-	    	
-	    	// 업로드시 메시지 출력
-	    	printWriter.println("{\"filename\" : \"" + fileName + "\", \"uploaded\" : 1, \"url\":\"" + fileUrl + "\"}");
-	    	printWriter.flush();
-	    	
-	    	}
-	    	catch(IOException e){
-	    		e.printStackTrace();
-	    	} 
-	    	finally {
-	    		try {
-	    		if(out != null) { out.close(); }
-	    		if(printWriter != null) { printWriter.close(); }
-	    	} 
-	    		catch(IOException e) { e.printStackTrace(); }
-	    	}
-	    	return;
-	    }
+		 @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+		    public void imageUpload(Model model, HttpServletRequest request, HttpServletResponse response, MultipartHttpServletRequest multiFile
+		    		, @RequestParam MultipartFile upload) throws Exception{
+		    	// 랜덤 문자 생성
+		    	UUID uid = UUID.randomUUID();
+		    	
+		    	OutputStream out = null;
+		    	PrintWriter printWriter = null;
+		    	
+		    	//인코딩
+		    	response.setCharacterEncoding("utf-8");
+		    	response.setContentType("text/html;charset=utf-8");
+		    	try{
+		    		//파일 이름 가져오기
+		    		String fileName = upload.getOriginalFilename();
+		    		byte[] bytes = upload.getBytes();
+		    		
+		    		//이미지 경로 생성
+		    		String path = "C:\\ckeditor_upload_" + "ckImage/";
+		    		String ckUploadPath = path + uid + "_" + fileName;
+		    		File folder = new File(path);
+		    		System.out.println("path : " + path);	// 이미지 저장경로 console에 확인
+		    		
+		    		//해당 디렉토리 확인
+		    		if(!folder.exists()){
+		    			try{
+		    				folder.mkdirs(); // 폴더 생성
+		    		}catch(Exception e){
+		    			e.getStackTrace();
+		    		}
+		    	}
+		    	
+		    	out = new FileOutputStream(new File(ckUploadPath));
+		    	out.write(bytes);
+		    	out.flush(); // outputStram에 저장된 데이터를 전송하고 초기화
+		    	
+		    	String callback = request.getParameter("CKEditorFuncNum");
+		    	printWriter = response.getWriter();
+		    	String fileUrl = "/a_board/ckImgSubmit?uid=" + uid + "&fileName=" + fileName; // 작성화면
+		    	
+		    	// 업로드시 메시지 출력
+		    	printWriter.println("{\"filename\" : \"" + fileName + "\", \"uploaded\" : 1, \"url\":\"" + fileUrl + "\"}");
+		    	printWriter.flush();
+		    	
+		    	}
+		    	catch(IOException e){
+		    		e.printStackTrace();
+		    	} 
+		    	finally {
+		    		try {
+		    		if(out != null) { out.close(); }
+		    		if(printWriter != null) { printWriter.close(); }
+		    	} 
+		    		catch(IOException e) { e.printStackTrace(); }
+		    	}
+		    	return;
+		    }
 
-	 	//서버로 전송된 이미지 뿌려주기
-	    @RequestMapping(value = "/ckImgSubmit")
-	    public void ckSubmit(@RequestParam(value = "uid") String uid, @RequestParam(value="fileName") String fileName
-	    		, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-	    	
-	    	//서버에 저장된 이미지 경로
-	    	String path = "C:\\ckeditor_upload_" + "ckImage/";	// 저장된 이미지 경로
-	    	System.out.println("path : " + path);
-	    	String sDirPath = path + uid + "_" + fileName;
-	    	
-	    	File imgFile = new File(sDirPath);
-	    	
-	    	//사진 이미지 찾지 못하는 경우 예외처리로 빈 이미지 파일을 설정한다.
-	    	if(imgFile.isFile()) {
-	    		byte[] buf = new byte[1024];
-	    		int readByte = 0;
-	    		int length = 0;
-	    		byte[] imgBuf = null;
-	    		
-	    		FileInputStream fileInputStream = null;
-	    		ByteArrayOutputStream outputStream = null;
-	    		ServletOutputStream out = null;
-	    		
-	    		try {
-	    			fileInputStream = new FileInputStream(imgFile);
-	    			outputStream = new ByteArrayOutputStream();
-	    			out = response.getOutputStream();
-	    			
-	    			while((readByte = fileInputStream.read(buf)) != -1){
-	    				outputStream.write(buf, 0, readByte); 
-	    			}
-	    			
-	    			imgBuf = outputStream.toByteArray();
-	    			length = imgBuf.length;
-	    			out.write(imgBuf, 0, length);
-	    			out.flush();
-	    			
-	    		}
-	    		catch(IOException e){
-	    			e.printStackTrace();
-	    		}
-	    		finally {
-	    			outputStream.close();
-	    			fileInputStream.close();
-	    			out.close();
-	   			}
-	   		}
-	    }
+		 	//서버로 전송된 이미지 뿌려주기
+		    @RequestMapping(value = "/ckImgSubmit")
+		    public void ckSubmit(@RequestParam(value = "uid") String uid, @RequestParam(value="fileName") String fileName
+		    		, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		    	
+		    	//서버에 저장된 이미지 경로
+		    	String path = "C:\\ckeditor_upload_" + "ckImage/";	// 저장된 이미지 경로
+		    	System.out.println("path : " + path);
+		    	String sDirPath = path + uid + "_" + fileName;
+		    	
+		    	File imgFile = new File(sDirPath);
+		    	
+		    	//사진 이미지 찾지 못하는 경우 예외처리로 빈 이미지 파일을 설정한다.
+		    	if(imgFile.isFile()) {
+		    		byte[] buf = new byte[1024];
+		    		int readByte = 0;
+		    		int length = 0;
+		    		byte[] imgBuf = null;
+		    		
+		    		FileInputStream fileInputStream = null;
+		    		ByteArrayOutputStream outputStream = null;
+		    		ServletOutputStream out = null;
+		    		
+		    		try {
+		    			fileInputStream = new FileInputStream(imgFile);
+		    			outputStream = new ByteArrayOutputStream();
+		    			out = response.getOutputStream();
+		    			
+		    			while((readByte = fileInputStream.read(buf)) != -1){
+		    				outputStream.write(buf, 0, readByte); 
+		    			}
+		    			
+		    			imgBuf = outputStream.toByteArray();
+		    			length = imgBuf.length;
+		    			out.write(imgBuf, 0, length);
+		    			out.flush();
+		    			
+		    		}
+		    		catch(IOException e){
+		    			e.printStackTrace();
+		    		}
+		    		finally {
+		    			outputStream.close();
+		    			fileInputStream.close();
+		    			out.close();
+		   			}
+		   		}
+		    }
 
 	//게시판 목록
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
@@ -387,7 +387,7 @@ public class ABoardController {
 		//관리자가 신고된 게시글 삭제
 		@RequestMapping(value = "/admin_delete", method = RequestMethod.GET)
 		@ResponseBody
-		public boolean admin_delete(@RequestParam(value = "a_bno") int a_bno) throws Exception {
+		public boolean admin_delete(@ModelAttribute ABoardVO boardVO, @RequestParam(value = "a_bno", required = false) int a_bno) throws Exception {
 
 			logger.info("관리자가 신고된 게시글 삭제");
 			
@@ -401,6 +401,30 @@ public class ABoardController {
 			map.put("board_type", "a_board");
 			
 			complaintService.delete_complaint(map);
+			
+			//신고된 게시글 작성자 경고 횟수 조회(caution값 조회)
+			System.out.println("id : " + boardVO.getId());
+			int count = complaintService.count_caution(boardVO.getId());
+			
+			if(count < 4) {
+				complaintService.add_caution(boardVO.getId());
+				
+				String content = "회원님이 작성한 커뮤니티 게시물은 부적접한 사유로 인해 삭제되었습니다."
+	 					+ "\n회원님은 누적 경고수는 " + ++count + "입니다."
+	 					+ "\n누적 경고수가 5가 되면 회원 강제 탈퇴가 이루어집니다.";
+			
+				Map<String, Object> map2 = new HashMap<>();
+				
+				map2.put("n_receiver", boardVO.getId());
+				map2.put("n_content", content);
+				
+				complaintService.note_caution(map2);
+			}
+			else {
+				String email = complaintService.expel_email(boardVO.getId());
+				complaintService.expel_member(boardVO.getId());
+				complaintService.insert_email(email);
+			}
 			
 			boolean result = true;
 			return result;
