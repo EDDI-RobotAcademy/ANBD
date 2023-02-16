4bd49c450e7c5c027bddd58978d78a9050b21cc2
eopu <121409050+eopu@users.noreply.github.com>
2023-02-16 16:30:39 +0900

    fix: main 수정

--- anabada.zip_expanded/anabada/src/main/java/com/anabada/web/dao/AdminBannerDAOImpl.java
+++ anabada.zip_expanded/anabada/src/main/java/com/anabada/web/dao/AdminBannerDAOImpl.java
@@ -5,6 +5,8 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.anabada.web.vo.BannerVO;
+
 @Repository
 public class AdminBannerDAOImpl implements AdminBannerDAO {
 
